d46d74b7bd7b324d44f1a047b87b90c5c6951f03
shaulov <shineginn@gmail.com>
2022-09-27 16:32:39 +0300

    Fix posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -2,10 +2,7 @@ import Heading from "../components/heading";
 
 function Posts () {
   return (
-    <>
-      <Heading tag={'h1'} text={'Posts list'} />
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, fuga reiciendis magni qui molestiae tempore nulla maxime, amet exercitationem sapiente esse quae voluptatem veritatis blanditiis nisi, facere dolorum alias. Labore!</p>
-    </>
+    <Heading tag={'h1'} text={'Posts list:'} />
   );
 }
 
