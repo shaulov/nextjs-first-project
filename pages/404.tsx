321d16c5ac97f2dea231160764a0d8583032a971
shaulov <shineginn@gmail.com>
2022-09-27 16:36:26 +0300

    Add timeout for redirecting on home page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Heading from '../components/heading';
+import { AppRoute } from '../const';
 import styles from '../styles/404.module.scss';
 
 function Error () {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push(AppRoute.Root);
+    }, 3000);
+  }, [router]);
+
   return (
     <div className={styles.wrapper}>
       <div>
