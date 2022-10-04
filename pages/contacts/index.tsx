a1a2fedf5a1001d1177edd381f5c6ae587284d6a
shaulov <shineginn@gmail.com>
2022-10-04 10:32:56 +0300

    Add data fetching when page renders

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
 import Heading from "../../components/heading";
 
 function Contacts () {
+  const [users, setUsers] = useState(null);
+
+  const { fetchUsers } = api();
+
+  useEffect(() => {
+    fetchUsers()
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <>
       <Head>
