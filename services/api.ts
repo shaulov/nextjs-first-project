27dc4d47907856a3bd6e6b04a4e6d4389d271b6a
shaulov <shineginn@gmail.com>
2022-10-04 13:17:01 +0300

    Add posts and single post data fetching

--- services/api.ts
+++ services/api.ts
@@ -22,6 +22,22 @@ const useApiService = () => {
       const response = await fetch(`${_apiBase}/users/${id}`);
 
       if (!response.ok) {
+        return null;
+      }
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(`${_apiBase}/posts`);
+
+      if (!response.ok) {
         throw new Error(`Couldn't fetch ${response.url}, status: ${response.status}`);
       }
 
@@ -33,7 +49,23 @@ const useApiService = () => {
     }
   }
 
-  return { fetchUsers, fetchUser };
+  const fetchPost = async (id: string) => {
+    try {
+      const response = await fetch(`${_apiBase}/posts/${id}`);
+
+      if (!response.ok) {
+        return null;
+      }
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return { fetchUsers, fetchUser, fetchPosts, fetchPost };
 }
 
 export default useApiService;
