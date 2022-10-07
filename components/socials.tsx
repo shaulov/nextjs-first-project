4b14d194d3f98132cbf2f448b564fc11b646db65
shaulov <shineginn@gmail.com>
2022-10-07 15:34:18 +0300

    Connect font awesome for icons style

--- components/socials.tsx
+++ components/socials.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { SocialData } from '../types/social-data';
 import styles from '../styles/Socials.module.scss';
 
@@ -11,15 +12,20 @@ function Socials ({ socials }: SocialsProps) {
   }
 
   return (
-    <ul className={styles.socials}>
-      {socials && socials.map(({ id, icon, path }) => (
-        <li key={id}>
-          <a href={path} target="_blank" rel='noopener noreferrer'>
-            <i className={`fab fa-${icon}`} aria-hidden="true" />
-          </a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/fontawesome.min.css" integrity="sha512-RvQxwf+3zJuNwl4e0sZjQeX7kUa3o82bDETpgVCH2RiwYSZVDdFJ7N/woNigN/ldyOOoKw8584jM4plQdt8bhA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </Head>
+      <ul className={styles.socials}>
+        {socials && socials.map(({ id, icon, path }) => (
+          <li key={id}>
+            <a href={path} target="_blank" rel='noopener noreferrer'>
+              <i className={`fab fa-${icon}`} aria-hidden="true" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
